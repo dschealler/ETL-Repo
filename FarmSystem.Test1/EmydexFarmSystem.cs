55cdbed5767a24a0d1958c765eee2816dfe1ead4
dschealler <daniel.schealler@gmail.com>
2022-02-16 18:15:19 +1300

    Updating farm to output animal noises to console.

--- FarmSystem.Test1/EmydexFarmSystem.cs
+++ FarmSystem.Test1/EmydexFarmSystem.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FarmSystem.Test1
 {
     public class EmydexFarmSystem
     {
+        private List<Animal> _animals = new List<Animal>();
+
         //TEST 1
         public void Enter(Animal animal)
         {
             //TODO Modify the code so that we can display the type of animal (cow, sheep etc) 
             //Hold all the animals so it is available for future activities
+            this._animals.Add(animal);
             Console.WriteLine(this.EnterLine(animal));
         }
 
@@ -21,7 +25,17 @@ namespace FarmSystem.Test1
         public void MakeNoise()
         {
             //Test 2 : Modify this method to make the animals talk
-            Console.WriteLine("There are no animals in the farm");
+            if (this._animals.Count == 0)
+            {
+                Console.WriteLine("There are no animals in the farm");
+            }
+            else
+            {
+                foreach(var animal in this._animals)
+                {
+                    Console.WriteLine(animal.MakeNoise());
+                }
+            }
         }
 
         //TEST 3
