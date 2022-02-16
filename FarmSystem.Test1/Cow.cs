a0f768e184b558cf37dc8444ceec9fd02af6243d
dschealler <daniel.schealler@gmail.com>
2022-02-16 18:09:01 +1300

    Unit tests passing.

--- FarmSystem.Test1/Cow.cs
+++ FarmSystem.Test1/Cow.cs
@@ -5,11 +5,13 @@ namespace FarmSystem.Test1
     public class Cow : Animal
     {
         private const string NAME = "Cow";
+        private const string NOISE = "Moo!";
 
         private string _id;
         private int _noOfLegs = 4;
 
         public override string Name => NAME;
+        public override string Noise => NOISE;
 
         public string Id
         {
